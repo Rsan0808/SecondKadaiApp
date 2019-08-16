cc4bc8121eee696430ce4fb015e8b5168e50d5ed
Rsan0808 <rsandane@gmail.com>
2019-08-16 14:24:00 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by Rsan on 2019/08/16.
+//  Copyright © 2019 Rsan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
